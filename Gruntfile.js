6e55e71d52f5142904713824b66b031e5b48bfa8
Anthony Grimes <i@raynes.me>
2015-04-09 14:36:24 -0700

    Revert "Just use babel's transpiling on the fly."
    This reverts commit 504872f94b17140b06cd06915e2d25fb1f6d8d91.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,25 @@ module.exports = function(grunt) {
   require("load-grunt-tasks")(grunt);
 
   grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    "babel": {
+      options: {
+        sourceMap: true,
+        experimental: true,
+        playground: true
+      },
+      dist: {
+        files: [
+          {
+            expand: true,
+            cwd: 'src',
+            src: ['**/*.js'],
+            dest: 'lib'
+          }
+        ]
+      }
+    },
+
     'gh-pages': {
       options: {
         base: 'doc'
